19add2efc264f45a9a23ac318d59ebce01112f7d
AasharAzeem <aashar.azeem@webgalaxie.com>
2021-02-09 15:51:34 +0100

    Login

--- module/User/src/Model/Table/UsersTable.php
+++ module/User/src/Model/Table/UsersTable.php
@@ -6,14 +6,18 @@ namespace User\Model\Table;
 use Laminas\Crypt\Password\Bcrypt;
 use Laminas\Db\Adapter\Adapter;
 use Laminas\Db\TableGateway\AbstractTableGateway;
+use Laminas\Filter\StringToLower;
 use Laminas\Filter\StringTrim;
 use Laminas\Filter\StripTags;
+use Laminas\Filter\ToInt;
 use Laminas\I18n\Filter\Alnum;
+use Laminas\I18n\Validator\IsInt;
 use Laminas\InputFilter\Factory;
 use Laminas\InputFilter\InputFilter;
 use Laminas\Validator\Csrf;
 use Laminas\Validator\Date;
 use Laminas\Validator\Db\NoRecordExists;
+use Laminas\Validator\Db\RecordExists;
 use Laminas\Validator\EmailAddress;
 use Laminas\Validator\Identical;
 use Laminas\Validator\InArray;
@@ -31,6 +35,134 @@ class UsersTable extends AbstractTableGateway{
         $this->initialize();
     }
 
+    public function getLoginFormInputFilter(){
+        $inputFilter = new InputFilter();
+        $inputFilterFactory = new Factory();
+
+
+        # filter and validate email input field
+        $inputFilter->add(
+            $inputFilterFactory->createInput(
+                [
+                    'name' => 'email',
+                    'required' => true,
+                    'filters' => [
+                        ['name' => StripTags::class],
+                        ['name' => StringTrim::class],
+                        ['name' => StringToLower::class],
+                    ],
+                    'validators' => [
+                        ['name' => NotEmpty::class],
+                        ['name' => EmailAddress::class],
+                        [
+                            'name' => StringLength::class,
+                            'options' => [
+                                'min' => 6,
+                                'max' => 128,
+                                'messages' => [
+                                    StringLength::TOO_SHORT => 'Email address must have at least 6 characters!',
+                                    StringLength::TOO_LONG => 'Email address must have at most 128 characters!',
+                                ],
+                            ],
+                        ],
+                        [
+                            'name' => RecordExists::class,
+                            'options' => [
+                                'table' => $this->table,
+                                'field' => 'email',
+                                'adapter' => $this->adapter,
+                            ],
+                        ],
+                    ],
+                ]
+            )
+        );
+
+
+        # filter and validate password input field
+        $inputFilter->add(
+            $inputFilterFactory->createInput(
+                [
+                    'name' => 'password',
+                    'required' => true,
+                    'filters' => [
+                        ['name' => StripTags::class], # stips html tags
+                        ['name' => StringTrim::class], # removes empty spaces
+                    ],
+                    'validators' => [
+                        ['name' => NotEmpty::class],
+                        [
+                            'name' => StringLength::class,
+                            'options' => [
+                                'min' => 8,
+                                'max' => 25,
+                                'messages' => [
+                                    StringLength::TOO_SHORT => 'Password must have at least 8 characters',
+                                    StringLength::TOO_LONG => 'Password must have at most 25 characters',
+                                ],
+                            ],
+                        ],
+                    ],
+                ]
+            )
+        );
+
+
+        # filter and validate Recall CheckBox
+        $inputFilter->add(
+            $inputFilterFactory->createInput(
+                [
+                    'name' => 'recall',
+                    'required' => true,
+                    'filters' => [
+                        ['name' => StripTags::class], # stips html tags
+                        ['name' => StringTrim::class], # removes empty spaces
+                        ['name' => ToInt::class],
+                    ],
+                    'validators' => [
+                        ['name' => NotEmpty::class],
+                        [
+                            'name' => IsInt::class,
+                        ],
+                        [
+                            'name' => InArray::class,
+                            'options' => [
+                                'haystack' => [0,1],
+                            ],
+                        ],
+                    ],
+                ]
+            )
+        );
+
+
+        # csrf field
+        $inputFilter->add(
+            $inputFilterFactory->createInput(
+                [
+                    'name' => 'csrf',
+                    'required' => true,
+                    'filters' => [
+                        ['name' => StripTags::class], # stips html tags
+                        ['name' => StringTrim::class], # removes empty spaces
+                    ],
+                    'validators' => [
+                        ['name' => NotEmpty::class],
+                        [
+                            'name' => Csrf::class,
+                            'options' => [
+                                'messages' => [
+                                    Csrf::NOT_SAME => 'Oops! Refill the form.',
+                                ],
+                            ],
+                        ],
+                    ],
+                ]
+            )
+        );
+
+        return $inputFilter;
+    }
     public function setCreateInputFilter(){
         $inputFilter = new InputFilter();
         $inputFilterFactory = new Factory();
